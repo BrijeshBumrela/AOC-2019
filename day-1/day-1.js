74fb5f348bde78f7b1592cbf8bd4bde61b37c742
BrijeshBumrela <brijeshbumrela@gmail.com>
2019-12-02 01:15:52 +0530

    part 2 of day-1

--- day-1/day-1.js
+++ day-1/day-1.js
@@ -1,10 +1,25 @@
 const fs = require('fs');
 
 const data = fs.readFileSync('./input.txt', 'utf-8');
-
-
 const inputs = data.split('\n');
-const findFuel = (mass) => Math.floor(mass / 3) - 2;
-const finalFuel = inputs.reduce((acc, input) => acc + findFuel(parseInt(input)), 0);
 
-console.log(finalFuel);
+
+// Part 1
+
+const findFuel = (mass) => Math.floor(mass / 3) - 2;
+// const finalFuel = inputs.reduce((acc, input) => acc + findFuel(parseFloat(input)), 0);
+
+
+// Part 2
+
+const findFuelForFuel = (fuel) => {
+    let sum = 0;
+    while (fuel > 0) {
+        fuel = findFuel(fuel);
+        sum += fuel >= 0 ? fuel : 0;
+    }
+    return sum;
+}
+
+const finalFuelRequired = inputs.reduce((acc, input) => acc + findFuelForFuel(parseInt(input)), 0);
+console.log(finalFuelRequired);
